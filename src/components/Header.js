5364e91d3c6e15dac75bae9340630950939dc39a
javiercubas <66169702+javiercubas@users.noreply.github.com>
2023-07-16 18:38:53 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>MATERIALES DE CONSTRUCCIÓN</h1>
+            <h1 className='titulo-header'>MATERIALES DE CONSTRUCCIÓN</h1>
         </div>
     )
 }
