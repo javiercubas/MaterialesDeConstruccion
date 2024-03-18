70cb144e46bcdb30ce5e6801cb5b9bfdd96ea71d
javiercubas <66169702+javiercubas@users.noreply.github.com>
2024-03-18 10:39:47 +0100

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../components/Menu.css';
+import './Home.css'
 import Buscador from '../components/Buscador';
 import Productos from '../components/Productos';
 import { getOfertas } from '../modelos/ProductoModel';
 import PromoPopUp from '../components/PromoPopUp';
 import ThermoRossiSVG from '../components/thermorossi'
+import { getCategorias } from '../modelos/CategoriaModel';
 
 const Home = () => {
   const [productosOferta, setProductosOferta] = useState([]);
@@ -22,10 +24,29 @@ const Home = () => {
     window.location.href = 'https://www.ifema.es/construtec';
   }
 
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    getCategorias().then((categorias) => {
+      setCategorias(categorias);
+    });
+  }
+    , []);
+
   return (
     <>
       <Buscador />
       <Productos titulo="Ofertas" productos={productosOferta} />
+      <div className='categorias-section'>
+        <h2>Categorías</h2>
+        <div className='categorias'>
+          {
+            categorias.map((categoria, index) => (
+              <a key={index} href={`/productos/${categoria.nombre.toLowerCase().trim().replaceAll(' ', '-')}`}>{categoria.nombre}</a>
+            ))
+          }
+        </div>
+      </div>
       <div className="sponsors-content" onClick={handleShowPopup}>
         <h3 className="sponsors-titulo">promo</h3>
         <ThermoRossiSVG />
