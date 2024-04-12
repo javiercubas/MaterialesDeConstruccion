4ea746934b9d307c058ca2c2d41886bd5b7555c8
javiercubas <66169702+javiercubas@users.noreply.github.com>
2024-04-12 10:27:45 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,9 +7,11 @@ const Card = (props) => {
   // Limitar el título a 50 caracteres y agregar "..." si es más largo
   const limitedTitle = name.length > 50 ? name.substring(0, 50) + "..." : name;
 
+  // Codificar la URL de la imagen para evitar problemas con caracteres especiales
+  const imageEncoded = encodeURI(image);
   return (
     <a href={url} className="card-box">
-      <div className="foto-card" style={{ backgroundImage: `url(${image})` }} />
+      <div className="foto-card" style={{ backgroundImage: `url(${imageEncoded})` }} />
       <div className="inside-box">
         <h3 className="title-card">{limitedTitle}</h3>
         <div className="precio-card">{`${price} €${(pack == 1 ? '' : '/ud')}`}</div>
